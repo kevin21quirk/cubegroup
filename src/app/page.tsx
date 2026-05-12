e75a317b4d8aec60f09316dba822631bc48613ae
kevin21quirk <kevin.s.quirk@gmail.com>
2026-05-12 18:06:50 +0100

    Redirect homepage to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 import { redirect } from 'next/navigation'
-// import { auth } from '@clerk/nextjs/server'
 
 export default async function Home() {
-  // TEMPORARY: Skip auth check and go directly to dashboard
-  // const { userId } = await auth()
-  // if (userId) {
-  //   redirect('/dashboard')
-  // } else {
-  //   redirect('/sign-in')
-  // }
-  
-  redirect('/dashboard')
+  // Redirect to login page
+  redirect('/login')
 }
