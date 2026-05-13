e144524c2fa43b38c18d7adbc60eceda109f0725
kevin21quirk <kevin.s.quirk@gmail.com>
2026-05-13 10:33:14 +0100

    Temporarily remove sidebar to isolate rendering issue

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 // import { auth } from '@clerk/nextjs/server'
 // import { redirect } from 'next/navigation'
-import { Sidebar } from '@/components/dashboard/sidebar'
+// import { Sidebar } from '@/components/dashboard/sidebar'
 import { Header } from '@/components/dashboard/header'
 
 // Force dynamic rendering since we use cookies for auth
@@ -19,7 +19,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
+      {/* Temporarily removed sidebar */}
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
         <main className="flex-1 overflow-y-auto bg-gray-50 dark:bg-gray-900 p-6">
