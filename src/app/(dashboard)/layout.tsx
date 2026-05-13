aa530c5be9146e75545fa95c776302d37ac80580
kevin21quirk <kevin.s.quirk@gmail.com>
2026-05-13 10:51:45 +0100

    Remove sidebar to isolate server error

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 // import { auth } from '@clerk/nextjs/server'
 // import { redirect } from 'next/navigation'
-import { Sidebar } from '@/components/dashboard/sidebar'
+// import { Sidebar } from '@/components/dashboard/sidebar'
 import { Header } from '@/components/dashboard/header'
 
 export default function DashboardLayout({
@@ -16,7 +16,6 @@ export default function DashboardLayout({
 
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
         <main className="flex-1 overflow-y-auto bg-gray-50 dark:bg-gray-900 p-6">
